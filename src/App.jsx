9bc15f89e3aca95409e337d81105584e32bc0ab0
Prathamesh Dukare <Prathameshdukaremail@gmail.com>
2023-03-13 11:51:35 +0530

    accessing the state

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,28 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { useDispatch } from 'react-redux'
 import { addUser } from './store/slices/slice'
+import { useSelector } from 'react-redux'
 
 function App() {
   const dispatch = useDispatch()
+  const data = useSelector(state => state.users)
+  console.log(data);
 
-  const handleVite  = ()=>{
+  const handleVite = () => {
     console.log("adding vite user")
     dispatch(addUser(`viteuser_id_${Math.random()}`))
   }
 
-  const handleReact  = ()=>{
+  const handleReact = () => {
     console.log("adding react user")
     dispatch(addUser(`reactuser_id_${Math.random()}`))
   }
 
   return (
     <div className="App">
-      <div>
+      <div style={
+        { display: "flex" }
+      }>
         <p onClick={handleVite}>
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </p>
@@ -27,7 +32,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </p>
       </div>
-    </div>
+
+      <div className="users">
+      <h2>all users</h2>
+        {
+          data.map((user) => <p>{user}</p>)
+        }
+      </div>
+    </div >
   )
 }
 
